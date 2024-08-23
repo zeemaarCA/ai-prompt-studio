4de38b56d1fe4523da3a7aad7622493c8d4945a8
zeemaarCA <zeemaarca@gmail.com>
2024-08-23 21:09:30 +0500

    revert back database

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,31 +1,3 @@
-// import mongoose from 'mongoose';
-
-// let isConnected = false; // track the connection
-
-// export const connectToDB = async () => {
-//   mongoose.set('strictQuery', true);
-
-//   if(isConnected) {
-//     console.log('MongoDB is already connected');
-//     return;
-//   }
-
-//   try {
-//     await mongoose.connect(process.env.MONGODB_URI).then(() => {
-//       console.log('Connected to MongoDB');
-//     }).catch((err) => {
-//       console.log(err);
-//     });
-
-//     isConnected = true;
-
-//     console.log('MongoDB connected')
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
-
-
 import mongoose from 'mongoose';
 
 let isConnected = false; // track the connection
@@ -39,11 +11,11 @@ export const connectToDB = async () => {
   }
 
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      dbName: "prompt-studio",
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
+    await mongoose.connect(process.env.MONGODB_URI).then(() => {
+      console.log('Connected to MongoDB');
+    }).catch((err) => {
+      console.log(err);
+    });
 
     isConnected = true;
 
