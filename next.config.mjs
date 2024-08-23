d707b933e62e32635741512dab68f8feb04b04f1
zeemaarCA <zeemaarca@gmail.com>
2024-08-23 23:37:27 +0500

    revert back to default

--- next.config.mjs
+++ next.config.mjs
@@ -3,19 +3,6 @@ const nextConfig = {
   images: {
     domains: ['lh3.googleusercontent.com'],
   },
-  async headers() {
-    return [
-      {
-        source: "/",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "s-maxage=1, stale-while-revalidate=59",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
